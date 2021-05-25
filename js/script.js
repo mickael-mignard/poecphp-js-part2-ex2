83e906919daf8c0cdd18af288908ec44dedf5170
Mickael Mignard <mignard.mickael@gmail.com>
2021-05-25 09:12:40 +0200

    Mise au propre du code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-var inputLastName = document.getElementById("lastname");
+// Get input element
+let inputLastName = document.getElementById("lastname");
 
+// Add on input an event listener on blur to display a message
 inputLastName.addEventListener("blur", function(){
    alert("Merci de votre participation")  
 })
